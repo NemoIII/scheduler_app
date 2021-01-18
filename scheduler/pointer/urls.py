feab780dbcffb5f05c7d13599fecca91f04d9e55
Nemo <nemo@localhost.localdomain>
2021-01-18 19:19:27 +0000

    Adding pages and fixing a fill things

--- scheduler/pointer/urls.py
+++ scheduler/pointer/urls.py
@@ -3,5 +3,11 @@ from . import views
 
 urlpatterns = [
     path("", views.home, name="home"),
-    path("contact.html", views.contact, name="contact"),   
+    path("contact.html", views.contact, name="contact"),
+    path("about.html", views.about, name="about"),
+    path("blog-sigle.html", views.blog_sigle, name="blog-sigle"),
+    path("blog.html", views.blog, name="blog"),
+    path("department.html", views.department, name="department"),
+    path("doctor.html", views.doctor, name="doctor"),
+    path("pricing.html", views.pricing, name="pricing"),
 ]
--- scheduler/pointer/views.py
+++ scheduler/pointer/views.py
@@ -4,6 +4,9 @@ from django.core.mail import send_mail
 def home(request):
     return render(request, "home.html", {})
 
+def about(request):
+    return render(request, "about.html", {})
+
 def contact(request):
     if request.method == "POST":
         name = request.POST["name"]
@@ -20,3 +23,18 @@ def contact(request):
         return render(request, "contact.html", {"name": name})
     else:
         return render(request, "contact.html", {})
+
+def blog_sigle(request):
+    return render(request, "blog-single.html", {})
+
+def blog(request):
+    return render(request, "blog.html", {})
+
+def department(request):
+    return render(request, "department.html", {})
+
+def doctor(request):
+    return render(request, "doctor.html", {})
+
+def pricing(request):
+    return render(request, "pricing.html", {})
