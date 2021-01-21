962b5dec57e57ac50b494512ddc45d785b171b01
Nemo <nemo@localhost.localdomain>
2021-01-21 20:02:32 +0000

    Heroku added

--- scheduler/scheduler/settings.py
+++ scheduler/scheduler/settings.py
@@ -1,4 +1,7 @@
 import os
+import django_heroku
+import dj_database_url
+from decouple import config
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -36,6 +39,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'scheduler.urls'
@@ -111,6 +115,8 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static/pointer'),
 ]
 
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
 # accounts.google.com/DisplayUnlockCaptcha
 # myaccount.google.com/apppasswords
 # https://myaccount.google.com/lesssecureapps
@@ -121,4 +127,6 @@ EMAIL_HOST_PASSWORD = "kiko25801a"
 EMAIL_USE_TLS = True
 # EMAIL_USE_SSL = False
 
+
+django_heroku.settings(locals())
 
